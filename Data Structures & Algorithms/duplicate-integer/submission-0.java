a58789ff07c634811e3e68a02246c5df38665dc0
Sivanantham Gnanavel <158702293+sivanantham-gnanavel@users.noreply.github.com>
2026-05-16 11:29:13 +0530

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        int p1 = 0, p2 = p1+1;
+        while(p2 < nums.length) {
+            if(nums[p1] == nums[p2]) {
+                return true;
+            }
+            p1++;
+            p2++;
+        }
+        return false;
+    }
+}
